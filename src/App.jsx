7743b2aed3e576217a8d5f6bb610668322adae18
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-31 11:43:26 +0900

    ItemAdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Counter from "./components/Counter";
-import InputText from "./components/inputText";
 import ItemAdd from "./components/ItemAdd";
 
 const App = () => {
   return (
     <div className="bg-red-200 min-h-screen flex justify-center items-center">
-      {/* <Counter /> */}
-      {/* <InputText /> */}
       <ItemAdd />
     </div>
   );
